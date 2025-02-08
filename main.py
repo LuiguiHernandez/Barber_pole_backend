7e91b58bb248753d1d2b64895b806520ff75fdad
LuiguiHernandez <luiguibuelvas43@gmail.com.com>
2025-02-07 22:08:51 -0500

    sele cambio el puerto de escucha del archivo user al archivo main

--- main.py
+++ main.py
@@ -33,3 +33,7 @@ async def url():
 
 # Documentacion con Swagger: http://127.0.0.1:8000/doc
 # Documentacion con Redocly: http://127.0.0.1:8000/redoc
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)  
--- routers/user.py
+++ routers/user.py
@@ -89,8 +89,8 @@ def search_user (id: int):
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(router, host="0.0.0.0", port=8000)  
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(router, host="0.0.0.0", port=8000)  
     
-# documemntacion con Swagger http://127.0.0.1:8000/docs
+# # documemntacion con Swagger http://127.0.0.1:8000/docs
